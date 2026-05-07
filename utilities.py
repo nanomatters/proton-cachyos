e6d1bfcf37eff1e776e85c2ec0bfb8d5d9f5c785
Stelios Tsampas <loathingkernel@gmail.com>
2026-05-07 20:37:13 +0300

    utilities: update config to match protonfixes

--- utilities.py
+++ utilities.py
@@ -1,35 +1,42 @@
 """Various utility functions for use in the proton script"""
 
 import sys
+import os
 from argparse import Namespace
 from pathlib import Path
+
+base_config = Path(os.getenv('XDG_CONFIG_HOME', '~/.config')).expanduser()
+base_cache = Path(os.getenv('XDG_CACHE_HOME', '~/.cache')).expanduser()
 
 
 class Config(Namespace):
     class Path(Namespace):
-        cache_dir: Path = Path.home().joinpath('.cache', 'protonfixes')
+        config_dir: Path = base_cache.joinpath('protonfixes')
+        cache_dir: Path = base_cache.joinpath('protonfixes')
 
     path = Path()
 
 
-config = Config()
-
-
 class Log:
     @staticmethod
-    def warn(msg):
-        print('[Utilities] WARN: ' + msg, file=sys.stderr)
+    def info(msg):
+        sys.stderr.write('[Utilities] INFO: ' + msg)
+        sys.stderr.flush()
 
     @staticmethod
-    def info(msg):
-        print('[Utilities] INFO: ' + msg, file=sys.stderr)
+    def warn(msg):
+        sys.stderr.write('[Utilities] WARN: ' + msg)
+        sys.stderr.flush()
 
     @staticmethod
     def crit(msg):
-        print('[Utilities] ERROR: ' + msg, file=sys.stderr)
+        sys.stderr.write('[Utilities] ERROR: ' + msg)
+        sys.stderr.flush()
 
 
+config = Config()
 log = Log()
+
 
 if __name__ == "__main__":
     pass
