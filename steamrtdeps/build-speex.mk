91572ad3735169b61d3bf9e2217f82ef75627f2e
Stelios Tsampas <loathingkernel@gmail.com>
2026-07-05 23:14:18 +0300

    fixup! speex[dsp]: add and build submodules

--- steamrtdeps/build-speex.mk
+++ steamrtdeps/build-speex.mk
@@ -11,4 +11,8 @@ SPEEX_CONFIGURE_ARGS := \
 $(eval $(call rules-source,speex,$(SRCDIR)/steamrtdeps/speex))
 $(eval $(call rules-configure,speex,i386,unix))
 
+$(OBJ)/.speex-post-source:
+	cd "$(SPEEX_SRC)" && autoreconf -fiv
+	touch $@
+
 SPEEX_DEPENDENCY := speex
