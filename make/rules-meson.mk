75b3e1c2efcf12ed5510c8f3be2531b3eceaeaf8
Jacek Caban <jacek@codeweavers.com>
2026-08-06 18:42:40 +0200

    makefile: Fix passing linker flags to Meson projects.
    Use c_link_args and cpp_link_args instead of the ignored link_args key,
    and emit an empty array for empty flag lists.
    
    Preserve our per-target compiler flags and existing linker flag set.
    
    (cherry picked from commit 02b929785307e94ddbd53a040a9618a05a34247b)

--- make/rules-meson.mk
+++ make/rules-meson.mk
@@ -24,7 +24,8 @@ pkgconfig = '$$$$PKG_CONFIG'
 needs_exe_wrapper = true
 c_args = [$$(call list-quote,$$($(2)_$(3)_INCFLAGS) $$($(2)_CFLAGS) $$($(3)_CFLAGS) $$(CFLAGS) $$($(2)_$(3)_CFLAGS))]
 cpp_args = [$$(call list-quote,$$($(2)_$(3)_INCFLAGS) $$($(2)_CFLAGS) $$($(3)_CFLAGS) $$(CFLAGS) $$($(2)_$(3)_CFLAGS))]
-link_args = [$$(call list-quote,$$($(2)_$(3)-$(4)_LIBFLAGS) $$($(2)_$(3)_LIBFLAGS) $$($(2)_LDFLAGS) $$($(3)_LDFLAGS) $$(LDFLAGS))]
+c_link_args = [$$(call list-quote,$$($(2)_$(3)-$(4)_LIBFLAGS) $$($(2)_$(3)_LIBFLAGS) $$($(2)_LDFLAGS) $$($(3)_LDFLAGS) $$(LDFLAGS))]
+cpp_link_args = [$$(call list-quote,$$($(2)_$(3)-$(4)_LIBFLAGS) $$($(2)_$(3)_LIBFLAGS) $$($(2)_LDFLAGS) $$($(3)_LDFLAGS) $$(LDFLAGS))]
 pkg_config_libdir = '$$$$PKG_CONFIG_LIBDIR'
 
 [host_machine]
